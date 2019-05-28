1321e577d9b9fa3e755ce6566ee7cdee20ef233b
Priyank Singh <priyanksingh8@gmail.com>
2019-05-29 02:53:34 +0530

    Production Build, ready to deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 
-app.use(favicon(__dirname+ '/build/favicon.ico'));
+app.use(favicon(__dirname+ '/build/favicon.png'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'build')));
 app.get('/*', (req, res) => {
