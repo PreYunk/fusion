405b147475da7c9ac7912337106a07a4675b490e
Priyank Singh <priyanksingh8@gmail.com>
2019-06-12 00:10:58 +0530

    Updates

---
+++ precache-manifest.0085f8773da816cbe80db62f1c6142b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d482e95a5404a185e717bbd2883a21b1",
+    "url": "/fusion/index.html"
+  },
+  {
+    "revision": "1a1e2eadf828e041f9f7",
+    "url": "/fusion/static/css/main.a33f72e0.chunk.css"
+  },
+  {
+    "revision": "e7f8e7ce5af5d9d46337",
+    "url": "/fusion/static/js/2.06646780.chunk.js"
+  },
+  {
+    "revision": "1a1e2eadf828e041f9f7",
+    "url": "/fusion/static/js/main.174dd5a4.chunk.js"
+  },
+  {
+    "revision": "3f23b7e080a4852acacf",
+    "url": "/fusion/static/js/runtime~main.bb9caf1f.js"
+  },
+  {
+    "revision": "ef58919da2c5990f77d70e346d6b3bfe",
+    "url": "/fusion/static/media/logo.ef58919d.svg"
+  },
+  {
+    "revision": "32fe707feb2deb474ee6a84d5ec6212e",
+    "url": "/fusion/static/media/ytsans.32fe707f.woff2"
+  }
+]);
