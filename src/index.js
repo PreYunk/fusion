ee0eaf5009d9dc237a924ccfed2574d14d0a5beb
Priyank Singh <priyanksingh8@gmail.com>
2020-05-01 15:04:57 +0530

    Before starting v2.0

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
-import {createStore, applyMiddleware, compose, combineReducers} from "redux";
-import {Provider} from "react-redux";
-import thunk from 'redux-thunk';
-import addQuestionReducer from './store/reducers/addQuestion';
-import generatePaperReducer from './store/reducers/generatePaper';
-import selectQuestionsReducer from './store/reducers/selectQuestions';
-import loginReducer from './store/reducers/login';
-import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import addQuestionReducer from "./store/reducers/addQuestion";
+import generatePaperReducer from "./store/reducers/generatePaper";
+import selectQuestionsReducer from "./store/reducers/selectQuestions";
+import loginReducer from "./store/reducers/login";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
 
 // axios.defaults.baseURL = 'http://localhost:3001/api';
-axios.defaults.baseURL = 'https://polar-sea-14304.herokuapp.com/api';
+axios.defaults.baseURL = "https://polar-sea-14304.herokuapp.com/api";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
-   addQuestionReducer,
-   generatePaperReducer,
-    selectQuestionsReducer,
-    loginReducer
+  addQuestionReducer,
+  generatePaperReducer,
+  selectQuestionsReducer,
+  loginReducer,
 });
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-const app = window.innerWidth >= 500 ?
+const app =
+  window.innerWidth >= 700 ? (
     <Provider store={store}>
-        <BrowserRouter basename="/fusion">
-            <App/>
-        </BrowserRouter>
-    </Provider>: <h1 style={{color: '#52E5AA'}}>THIS APP IS NOT SUPPORTED FOR MOBILE DEVICES YET </h1>;
+      <BrowserRouter basename="/fusion">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  ) : (
+    <h1 style={{ color: "#52E5AA" }}>
+      THIS APP IS NOT SUPPORTED FOR MOBILE DEVICES YET{" "}
+    </h1>
+  );
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
