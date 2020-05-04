59a0292d9288a8a4a158e058aa9a50a22eb9a8a4
Priyank Singh <priyanksingh8@gmail.com>
2020-05-04 19:01:30 +0530

    Optimised AddQuestion and ViewQuestion for mobile [Fix needed for editor]

--- src/components/MaterialComponents/MaterialFab/MaterialFab.js
+++ src/components/MaterialComponents/MaterialFab/MaterialFab.js
@@ -1,29 +1,46 @@
-import React from 'react';
-import {withStyles} from "@material-ui/core/styles";
-import Fab from '@material-ui/core/Fab';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
 
 const styles = () => ({
-   root: {
-       backgroundColor: '#313131',
-       color: '#f1f1f1',
-       margin : '0 30px',
-       fontFamily: 'Raleway',
-       fontWeight: '700',
-       width: '150px',
-       marginTop: '50px',
-       transition: 'transform 100ms ease-in',
-       '&:hover' : {
-           color: '#313131',
-           transform: 'scale(1.2)'
-       }
-   }
+  root: {
+    backgroundColor: "#313131",
+    color: "#f1f1f1",
+    margin: "0 30px",
+    fontFamily: "Raleway",
+    fontWeight: "700",
+    width: "150px",
+    marginTop: "50px",
+    transition: "transform 100ms ease-in",
+    "&:hover": {
+      color: "#313131",
+      transform: "scale(1.2)",
+    },
+  },
+  mobile: {
+    backgroundColor: "#313131",
+    color: "#f1f1f1",
+    fontFamily: "Raleway",
+    fontWeight: "700",
+    transition: "transform 100ms ease-in",
+    "&:hover": {
+      color: "#313131",
+      transform: "scale(1.2)",
+    },
+  },
 });
 
 const materialFab = (props) => {
-    const {classes} = props;
-    return (
-        <Fab onClick={props.onClick} variant='extended' classes={{root: classes.root}}>{props.children}</Fab>
-    )
+  const { classes } = props;
+  return (
+    <Fab
+      onClick={props.onClick}
+      variant="extended"
+      classes={{ root: props.mobile ? classes.mobile : classes.root }}
+    >
+      {props.children}
+    </Fab>
+  );
 };
 
 export default withStyles(styles)(materialFab);
--- src/components/RowGroupedThree/RowGroupedThree.js
+++ src/components/RowGroupedThree/RowGroupedThree.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import classes from './RowGroupedThree.css';
+import React from "react";
+import classes from "./RowGroupedThree.css";
 
 const rowGroupedThree = (props) => {
-    return (
-        <div className={classes.RowGroupedThree}>
-            {props.firstComp}
-            {props.secondComp}
-            {props.thirdComp}
-        </div>
-    );
+  return (
+    <div className={classes.RowGroupedThree}>
+      <div className={classes.wrapper}>
+        {props.firstComp}
+        {props.secondComp}
+        {props.thirdComp}
+      </div>
+    </div>
+  );
 };
 
 export default rowGroupedThree;
