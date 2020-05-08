15ab0f53f13e334740231de11a50de8e099633bb
Priyank Rajput <priyanksingh8@gmail.com>
2020-05-08 18:32:03 +0530

    Add files via upload

---
+++ precache-manifest.1614ecf3e9427ab761453eeb7e4e4bdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3639722fba39982fc9d4764fca8bce91",
+    "url": "/fusion/index.html"
+  },
+  {
+    "revision": "b71c8b63dad24bedae85",
+    "url": "/fusion/static/css/main.0a0decb4.chunk.css"
+  },
+  {
+    "revision": "6025c9a6c59f81c4e98d",
+    "url": "/fusion/static/js/2.9961b8df.chunk.js"
+  },
+  {
+    "revision": "b71c8b63dad24bedae85",
+    "url": "/fusion/static/js/main.341cc6b7.chunk.js"
+  },
+  {
+    "revision": "3f23b7e080a4852acacf",
+    "url": "/fusion/static/js/runtime~main.bb9caf1f.js"
+  },
+  {
+    "revision": "be29f3c5ccd30b97f9c394a02c9ce5d7",
+    "url": "/fusion/static/media/LEMONMILK-Regular.be29f3c5.otf"
+  },
+  {
+    "revision": "672608a05e1eec1bbe92bb8243792264",
+    "url": "/fusion/static/media/MADE TOMMY Regular_PERSONAL USE.672608a0.otf"
+  },
+  {
+    "revision": "32fe707feb2deb474ee6a84d5ec6212e",
+    "url": "/fusion/static/media/ytsans.32fe707f.woff2"
+  }
+]);
